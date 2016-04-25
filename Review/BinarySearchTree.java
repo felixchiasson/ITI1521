5778d51a42d9af6f9ccab227f7b44872883cdee3
Félix Chiasson <github@felixchiasson.ca>
2016-04-24 23:43:27 -0400

    In Order Traverse

--- Review/BinarySearchTree.java
+++ Review/BinarySearchTree.java
@@ -2,13 +2,17 @@
 *     File Name           :     BinarySearchTree.java
 *     Created By          :     Félix Chiasson (7138723)
 *     Creation Date       :     [2016-04-24 23:01]
-*     Last Modified       :     [2016-04-24 23:22]
+*     Last Modified       :     [2016-04-24 23:41]
 *     Description         :     Review of binary search tree with comments
 **************************************************************************************************/
 
 public class BinarySearchTree {
 
     Node root;
+
+    /**
+     * Add a node to the tree
+     **/
 
     public void addNode(int key, String name) {
         Node newNode = new Node(key, name);
@@ -60,7 +64,33 @@ public class BinarySearchTree {
         }
     }
 
+    /**
+     * In Order Traversal (Pre-Order selon les notes de cours)
+     * We aim for the smallest value first (start at first left child)
+     * When Null is reached, then move up a value.
+     **/
+
+    public void inOrderTraverseTree(Node focusNode) {
+
+        if (focusNode != null) {
+            inOrderTraverseTree(focusNode.leftChild);
+            System.out.println(focusNode);
+            inOrderTraverseTree(focusNode.rightChild);
+        }
+    }
+
     public static void main(String[] args) {
+
+        BinarySearchTree t = new BinarySearchTree();
+
+        t.addNode(25, "Boss");
+        t.addNode(3, "Vice President");
+        t.addNode(37, "Office Manager");
+        t.addNode(1, "Secretary");
+        t.addNode(19, "Sales Manager");
+        t.addNode(48, "Salesman 1");
+
+        t.inOrderTraverseTree(t.root);
 
     }
 
